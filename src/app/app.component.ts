c9c984d013680b69785a3a273bd952d1a7e02c94
10GO <mixelidzet@gmail.com>
2025-02-25 18:16:18 +0400

    fix - add pallet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'cs2-server';
+  theme = signal<'dark' | 'light' | 'red'>('dark')
+
+  constructor() {
+    this.setInitialTheme()
+  }
+
+  setInitialTheme(){
+    this.setTheme(this.detectPrefersColorScheme());
+  }
+
+  setTheme(theme: "dark" | "light" | 'red'){
+    document.body.setAttribute(
+      'data-theme',
+      theme
+    );
+    this.theme.set(theme)
+  }
+
+  detectPrefersColorScheme(): "light" | 'red' {
+    if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? "red" : "light";
+    } else {
+      return "light";
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      },
+      {
+        path: 'home',
+        loadChildren: ()=> import('./features/home/home.routes').then(m => m.HomeRoutes)
+      }
+    ]
+  },
+];
