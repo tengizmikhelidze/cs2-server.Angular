f19ae164be0b8141a89780cb8290fecbffbe1840
10GO <mixelidzet@gmail.com>
2025-02-25 18:48:18 +0400

    before material

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
