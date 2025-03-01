c5758ffbbf3d8236ae95fca3ac2c14ff34ce8cb4
10GO <mixelidzet@gmail.com>
2025-03-01 14:58:24 +0400

    feat - add shared input

--- src/app/components/authorization/authorization.component.ts
+++ src/app/components/authorization/authorization.component.ts
@@ -1,11 +1,13 @@
 import {Component, inject} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {AuthorizationOverlayService} from './services/authorization-overlay.service';
+import {TextInputComponent} from '../../shared/components/inputs/text-input/text-input.component';
 
 @Component({
   selector: 'app-authorization',
   imports: [
-    MatButton
+    MatButton,
+    TextInputComponent
   ],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss'
---
+++ src/app/shared/components/inputs/text-input/text-input.component.ts
@@ -0,0 +1,69 @@
+import {Component, forwardRef, input, model} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-text-input',
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatIcon
+  ],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextInputComponent),
+      multi: true
+    }
+  ]
+})
+export class TextInputComponent implements ControlValueAccessor {
+  label = input<string>("");
+  placeholder = input<string>("");
+  type = input<'text' | 'email' | 'password'>('text');
+  disabled = model<boolean>(false);
+  inputId = model<string>('');
+  inputClass = model<string>('');
+
+  value = '';
+
+  // Callback functions for changes and touched status
+  onChange: (value: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  // Called when the form model changes programmatically
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  // Registers a function to call when the control value changes
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  // Registers a function to call when the control is touched
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  // Optionally handle the disabled state
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+
+  onInputEvent(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.onInput(inputElement.value);
+  }
+
+  // Called when the user types into the input field
+  onInput(value: string): void {
+    this.value = value;
+    this.onChange(value);
+  }
+}
